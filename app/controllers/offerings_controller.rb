4aa17ee274b9b4423508e0ccad8fdcfbc6995e18
Andres Henriquez <rymbell@gmail.com>
2023-08-16 15:55:53 +0900

    updated devide and updated booking form

--- app/controllers/offerings_controller.rb
+++ app/controllers/offerings_controller.rb
@@ -1,4 +1,5 @@
 class OfferingsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @offerings = Offering.all
     @offering = Offering.new
